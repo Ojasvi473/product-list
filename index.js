0c2fb351e84772879e84ed580fa8ea0011e5f8f4
Ojasvi473 <117925527+Ojasvi473@users.noreply.github.com>
2023-07-31 20:01:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,242 @@
+var productList = [
+  {
+    id: "1",
+    name: "Men Navy Blue Solid Sweatshirt",
+    preview:
+      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/7579188/2018/11/5/08a7b230-ee8f-46c0-a945-4e835a3c01c01541402833619-United-Colors-of-Benetton-Men-Sweatshirts-1271541402833444-1.jpg",
+
+    description:
+      "Navy solid sweatshirt with patchwork, has a round neck, long sleeves, straight hem",
+    isAccessory: false,
+    brand: "United Colors of Benetton",
+    price: "Rs 2599",
+  },
+  {
+    id: "2",
+    name: "Men Black MAMGP T7 Sweat Sporty Jacket",
+    preview:
+      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/2060340/2017/9/14/11505387708574-Puma-Men-Black-Solid-Sporty-Jacket-371505387708496-1.jpg",
+
+    description:
+      "Black solid sporty jacket, has a mock collar, 3 pockets, zip closure, long sleeves, straight hem, unlined lining",
+    size: [0, 0, 0, 1, 0],
+    isAccessory: false,
+    brand: "Puma",
+    price: "Rs 7999",
+  },
+  {
+    id: "3",
+    name: "Men Black Action Parkview Lifestyle Shoes",
+    preview:
+      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/productimage/2018/9/19/b1373b00-37d4-41a0-a908-5d742a68d3661537343756229-1.jpg",
+
+    description:
+      "Flaunt an effortless look with this ultimate pair of black coloured formal shoes from the house of Hush Puppies. Let the sun go down for uncomfortable shoes, as you opt for this pair of formal shoes that is crafted using comfortable TPR (thermoplastic rubber) sole. Pair these formal shoes with a shirt and trousers to look absolutely handsome",
+    size: [1, 1, 1, 1, 1],
+    isAccessory: false,
+    brand: "Hush Puppies",
+    price: "Rs 6999",
+  },
+  {
+    id: "4",
+    name: "Women Black Solid Lightweight Leather Jacket",
+    preview:
+      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/3854721/2018/3/12/11520844673915-BARESKIN-Women-Black-Solid-Lightweight-Leather-Jacket-7761520844673718-1.jpg",
+
+    description:
+      "Black solid leather jacket, has a spread collar, 3 pockets, zip closure, long sleeves, straight hem, polyester lining",
+    size: [1, 1, 1, 1, 1],
+    isAccessory: false,
+    brand: "BARESKIN",
+    price: "Rs 9999",
+  },
+  {
+    id: "5",
+    name: "Women Blue Solid Shirt Dress",
+    preview:
+      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/5649908/2018/5/10/6bfe80cd-2f55-42bc-aa7f-e0d6c9e2ac531525936414747-SASSAFRAS-Women-Blue-Solid-Shirt-Dress-3831525936414532-1.jpg",
+
+    description:
+      "Blue solid woven shirt dress, has a shirt collar, sleeveless, button closure, flared hem",
+    size: [1, 1, 1, 1, 1],
+    isAccessory: false,
+    brand: "SASSAFRAS",
+    price: "Rs 5200",
+  },
+  {
+    id: "6",
+    name: "Unisex Silver-Toned Series 3 Smart Watch",
+    preview:
+      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/9803247/2019/5/27/624dac60-4c42-4902-bba1-30a51cc7f43c1558948536543-Apple-Unisex-Smart-Watches-1441558948536225-1.jpg",
+
+    description:
+      "Low and high heart rate notifications. Emergency SOS. New Breathe watch faces. Automatic workout detection. New yoga and hiking workouts. Advanced features for runners like cadence and pace alerts. New head-to-head competitions. Activity sharing with friends. Personalized coaching. Monthly challenges and achievement awards. Walkie-Talkie, make phone calls, and send messages. Listen to Apple Music and Apple Podcasts. Use Siri in all-new ways. Silver aluminum case. Built-in GPS, GLONASS, Galileo, and QZSS. S3 with dual-core processor. W2 Apple wireless chip. Barometric altimeter Capacity 8GB. Optical heart sensor. 1 Year Manufacture Warranty",
+    size: [1, 1, 1, 1, 1, 1],
+    isAccessory: true,
+    brand: "Apple",
+    price: "Rs 31999",
+  },
+  {
+    id: "7",
+    name: "Unisex Black & Green Reflex 2.0 Smart Band",
+    preview:
+      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/productimage/2019/2/23/dc05b5e5-aa82-493e-bf0b-0c309baf21021550920383101-1.jpg",
+
+    description:
+      "The new Fastrack Reflex 2.0 Activity Tracker comes with all the features one could ask for! It comes in three colours that you can use to accentuate your athleisure, casual or party attire. Not only does it look uber cool in this cool grey & electric blue accent, further the TPU (Thermoplastic Polyurethane) strap ensures it gives you a snugfit while staying light on your wrist! It is a fitness band with smartwatch features. The band has Steps, Distance and Calorie Tracker, Sleep Tracker, Call & SMS Alerts, OLED Display, 10 Days Power Reserve, Vibration Alarm, Sedentary Reminder and is IPX6 Water Resistant. This band is compatible with both IOS and Android OS. It has a secure 'Pin' based pairing with your phone, for better protection. And, all of this at a price that is hard match anywhere else with quality and promise of the brand Fastrack. Move on and get one for yourself!",
+    size: [1, 1, 1, 1, 1],
+    isAccessory: true,
+    brand: "Fastrack",
+    price: "Rs 1999",
+  },
+  {
+    id: "8",
+    name: "Unisex Black Galaxy Fit Fitness Band",
+    preview:
+      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/10122083/2019/6/24/e533a691-3908-41b0-8307-1928a37d4ec41561362162650-Samsung-Galaxy-Fit-4801561362161527-1.jpg",
+
+    description:
+      "Keep up your routine for even longer. With plenty of power to last throughout your daily workouts, you can pack in those longer hours to reach your fitness goals. What's more, you won't have to worry about disruptive battery discharge when tracking your activity.",
+    size: [1, 1, 1, 1, 1],
+    isAccessory: true,
+    brand: "Samsung",
+    price: "Rs 9990",
+  },
+  {
+    id: "9",
+    name: "Gear IconX Black Cord-free Fitness Earbuds",
+    preview:
+      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/8593217/2019/4/9/de2c21f3-6731-4c29-9369-692a486a1b8c1554802772926-Samsung-Gear-IconX-Black-Cord-free-Fitness-Earbuds-256155480-1.jpg",
+
+    description:
+      "Speak commands via Bixby or Google Voice and control your music with just a tap or swipe. Meet your fitness goals with speed, distance, calorie tracking and real-time voice guidance. Find the perfect fit with multiple sizes of ear-tips and wing-tips right in the box. iOS/Mac users: Samsung Gear IconX can be used to stream music or take calls from your iOS handset or tablet. Fitness Functions that require the use of Samsung S-Health app are not available for iOS. 5 hours of Bluetooth streaming, 7 hours of MP3 listening, 4 hours talk time. Warranty: 6 months. Warranty provided by the brand owner / manufacturer",
+    size: [1, 1, 1, 1, 1],
+    isAccessory: true,
+    brand: "Samsung",
+    price: "Rs 13990",
+  },
+  {
+    id: "10",
+    name: "White 2nd Gen AirPods with Charging Case",
+    preview:
+      "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/9803279/2019/5/27/6661d579-12ae-456b-b8f2-e78e5256a99c1558948436173-Apple-AirPods-with-Charging-Case-2nd-Gen-White-3831558948435-1.jpg",
+
+    description:
+      "AirPods with Charging Case: More than 24 hours listening time,3 up to 18 hours talk time8. AirPods (single charge): Up to 5 hours listening time,1 up to 3 hours talk time2. 15 minutes in the case equals up to 3 hours listening time4 or up to 2 hours talk time. Warranty: 1 year. Warranty provided by Brand/Manufacturer",
+    size: [1, 1, 1, 1, 1],
+    isAccessory: true,
+    brand: "Apple",
+    price: "Rs 14999",
+  },
+];
+
+var mainSection = document.getElementById("card-container");
+// Heading Section
+var headerTag = document.createElement("h2");
+headerTag.innerHTML = "Clothing for Men and Women";
+headerTag.className = "header-tag";
+mainSection.appendChild(headerTag);
+
+// Clothing Section
+var clothingSection = document.createElement("div");
+clothingSection.className = "cloths-id";
+
+for (var i = 0; i < productList.length; i++) {
+  if (productList[i].isAccessory === false) {
+    var clothCard = document.createElement("div");
+    clothCard.className = "cloth-card";
+
+    var cardLink = document.createElement("a");
+    cardLink.href = "#";
+    clothCard.appendChild(cardLink);
+
+    var imgDiv = document.createElement("div");
+    imgDiv.className = "img-Div";
+
+    var modelImage = document.createElement("img");
+    modelImage.className = "model-image";
+    modelImage.src = productList[i].preview;
+
+    let abcDiv = document.createElement("div");
+    abcDiv.className = "abc";
+
+    var detailsdiv = document.createElement("h3");
+    detailsdiv.className = "details-div";
+    detailsdiv.innerHTML = productList[i].name;
+
+    var cardHeading = document.createElement("h4");
+    cardHeading.className = "card-header";
+    cardHeading.innerHTML = productList[i].brand;
+
+    var cardPrice = document.createElement("h5");
+    cardPrice.className = "card-price";
+    cardPrice.innerHTML = productList[i].price;
+    // cardPrice.innerHTML = Rs;
+    abcDiv.appendChild(detailsdiv);
+    abcDiv.appendChild(cardHeading);
+    abcDiv.appendChild(cardPrice);
+
+    imgDiv.appendChild(modelImage);
+
+    clothCard.appendChild(imgDiv);
+    clothCard.appendChild(abcDiv);
+
+    clothingSection.appendChild(clothCard);
+  }
+}
+mainSection.appendChild(clothingSection);
+// ----------------------------------------------------
+var mainSection = document.getElementById("card-container");
+// Heading Section
+var headerTag = document.createElement("h2");
+headerTag.innerHTML = "Accessories for Men and Women";
+headerTag.className = "header-tag";
+mainSection.appendChild(headerTag);
+
+// Clothing Section
+var clothingSection = document.createElement("div");
+clothingSection.className = "cloths-id";
+
+for (var i = 0; i < productList.length; i++) {
+  if (productList[i].isAccessory === true) {
+    var clothCard = document.createElement("div");
+    clothCard.className = "cloth-card";
+
+    var cardLink = document.createElement("a");
+    cardLink.href = "#";
+    clothCard.appendChild(cardLink);
+
+    var imgDiv = document.createElement("div");
+    imgDiv.className = "img-Div";
+
+    var modelImage = document.createElement("img");
+    modelImage.className = "model-image";
+    modelImage.src = productList[i].preview;
+
+    let abcDiv = document.createElement("div");
+    abcDiv.className = "abc";
+
+    var detailsdiv = document.createElement("h3");
+    detailsdiv.className = "details-div";
+    detailsdiv.innerHTML = productList[i].name;
+
+    var cardHeading = document.createElement("h4");
+    cardHeading.className = "card-header";
+    cardHeading.innerHTML = productList[i].brand;
+
+    var cardPrice = document.createElement("h5");
+    cardPrice.className = "card-price";
+    cardPrice.innerHTML = productList[i].price;
+
+    abcDiv.appendChild(detailsdiv);
+    abcDiv.appendChild(cardHeading);
+    abcDiv.appendChild(cardPrice);
+
+    imgDiv.appendChild(modelImage);
+
+    clothCard.appendChild(imgDiv);
+    clothCard.appendChild(abcDiv);
+    clothingSection.appendChild(clothCard);
+  }
+}
+mainSection.appendChild(clothingSection);
